9fd727d02249ac44de1f7a104c50888dd693f79e
Jon Chen <bsd@voltaire.sh>
2024-06-02 14:37:00 -0700

    get this thing working

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <TFT_eSPI.h>
+TFT_eSPI tft = TFT_eSPI();
 
 void setup()
 {
-	// put your setup code here, to run once:
-	Serial.begin(9600);
 	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, HIGH);
+	tft.init();
+	tft.setRotation(1);
+	tft.fillScreen(TFT_BLACK);
+	tft.setCursor(0, 0, 2);
+	tft.setTextColor(TFT_WHITE);
+	tft.println("Hello World!");
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
-	Serial.println("Hello World");
-	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+	Serial.println("Hello World!");
+	String message = "MISO is " + String(TFT_MISO) + " MOSI is " + String(TFT_MOSI) + " SCLK is " + String(TFT_SCLK) + " CS is " + String(TFT_CS) + " DC is " + String(TFT_DC) + " RST is " + String(TFT_RST) + " BL is " + String(TFT_BL);
+	Serial.println(message);
 	delay(1000);
 }
