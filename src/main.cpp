e644ab839fd0587dedd67dad533df6fc0a4039cf
Jon Chen <bsd@voltaire.sh>
2024-06-04 22:48:07 -0700

    configure arduino ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <WiFi.h>
 #include "secrets.h"
 #include <PubSubClient.h>
@@ -204,7 +205,7 @@ void ensureConnectedToWifi()
 		WiFi.noLowPowerMode();
 
 // set hostname if HOSTNAME is defined
-#if defined(HOSTNAME)
+#ifdef HOSTNAME
 		WiFi.setHostname(HOSTNAME);
 #endif
 
@@ -220,13 +221,50 @@ void ensureConnectedToWifi()
 
 void setup()
 {
+	// connect to wifi
 	ensureConnectedToWifi();
 	printCurrentNet();
 
+	// OTA setup
+	ArduinoOTA.onStart([]()
+										 {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type); });
+	ArduinoOTA.onEnd([]()
+									 { Serial.println("\nEnd"); });
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+												{ Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
+	ArduinoOTA.onError([](ota_error_t error)
+										 {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    } });
+	ArduinoOTA.begin();
+
+	// configure mqtt
 	mqttClient.setServer(BROKER_ADDRESS, 1883);
 
+	// turn on onboard LED to signal that we've started
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, HIGH);
+
+	// initialize TFT
 	tft.init();
 	set_brightness(20);
 	tft.setRotation(2);
@@ -288,12 +326,17 @@ void loop()
 	// ensure we're connected to the wifi
 	ensureConnectedToWifi();
 
+	// arduino OTA
+	ArduinoOTA.handle();
+
+	// ensure we're connected to the mqtt broker
 	if (!mqttClient.connected())
 	{
 		reconnect();
 	}
 	mqttClient.loop();
 
+	// start handling keypad
 	uint16_t t_x, t_y;
 	static uint16_t color;
 
